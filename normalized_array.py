3551b5f52ceea1b459fffb4e8ad855a458b72250
solmonhawa99-eng <solmonhawa99@gmail.com>
2026-04-08 15:14:25 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -9,6 +9,9 @@ def normalized_array(input_array):
     data = (data - np.min(data)) / (np.max(data) - np.min(data))
     return data
 
+print(normalized_array([5, 5, 5]))
+print(normalized_array([-1, 0, 1, 2]))
+
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
     test_data = [10, 20, 30, 40, 50]
